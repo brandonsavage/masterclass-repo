b3e46f73107285a1a0da54314f03950a5cf4214e
John Sposato <jsposato@gmail.com>
2015-02-08 21:23:30 -0500

    Adding new route definitions

--- config/config.php
+++ config/config.php
@@ -10,13 +10,17 @@ return array(
     ),
     
     'routes' => array(
-        '' => 'Jsposato\Controller\IndexController:index',
-        'story' => 'Jsposato\Controller\StoryController:index',
-        'story/create' => 'Jsposato\Controller\StoryController:create',
-        'comment/create' => 'Jsposato\Controller\CommentController:create',
-        'user/create' => 'Jsposato\Controller\UserController:create',
-        'user/account' => 'Jsposato\Controller\UserController:account',
-        'user/login' => 'Jsposato\Controller\UserController:login',
-        'user/logout' => 'Jsposato\Controller\UserController:logout',
+        '/' => ['class' => 'Jsposato\Controller\Index:index', 'type' => 'GET'],
+        '/story' => ['class' => 'Jsposato\Controller\StoryController:index', 'type' => 'GET'],
+        '/story/create' => ['class' => 'Jsposato\Controller\StoryController:create', 'type' => 'GET'],
+        '/story/create/save' => ['class' => 'Jsposato\Controller\StoryController:create', 'type' => 'POST'],
+        '/comment/create' => ['class' => 'Jsposato\Controller\CommentController:create', 'type' => 'POST'],
+        '/user/create' => ['class' => 'Jsposato\Controller\UserController:create', 'type' => 'GET'],
+        '/user/account/create' => ['class' => 'Jsposato\Controller\UserController:create', 'type' => 'POST'],
+        '/user/account' => ['class' => 'Jsposato\Controller\UserController:account', 'type' => 'GET'],
+        '/user/account/save' => ['class' => 'Jsposato\Controller\UserController:account', 'type' => 'POST'],
+        '/user/login/check' => ['class' => 'Jsposato\Controller\UserController:login', 'type' => 'POST'],
+        '/user/login' => ['class' => 'Jsposato\Controller\UserController:login', 'type' => 'GET'],
+        '/user/logout' => ['class' => 'Jsposato\Controller\UserController:logout', 'type' => 'GET']
     ),
 );
