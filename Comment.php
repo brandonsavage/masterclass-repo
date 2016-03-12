2fb18f246f3f8d00c98efda7066d83aeb814f8f7
Adam Altman <adam@ilabsinc.com>
2016-03-11 19:40:40 -0600

    Change Vagrantfile setup...
    - homestead error; reverted to rasmus' php7dev box
    - configured nginx and mysql

--- Comment.php
+++ Comment.php
@@ -1,21 +1,24 @@
 <?php
 
-class Comment {
-    
-    public function __construct($config) {
+class Comment
+{
+
+    public function __construct($config)
+    {
         $dbconfig = $config['database'];
         $dsn = 'mysql:host=' . $dbconfig['host'] . ';dbname=' . $dbconfig['name'];
         $this->db = new PDO($dsn, $dbconfig['user'], $dbconfig['pass']);
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
-    
-    public function create() {
-        if(!isset($_SESSION['AUTHENTICATED'])) {
+
+    public function create()
+    {
+        if (!isset($_SESSION['AUTHENTICATED'])) {
             die('not auth');
             header("Location: /");
             exit;
         }
-        
+
         $sql = 'INSERT INTO comment (created_by, created_on, story_id, comment) VALUES (?, NOW(), ?, ?)';
         $stmt = $this->db->prepare($sql);
         $stmt->execute(array(
@@ -25,5 +28,4 @@ class Comment {
         ));
         header("Location: /story/?id=" . $_POST['story_id']);
     }
-    
 }
