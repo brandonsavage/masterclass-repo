5b8b7553d1a28da65dbe42f14ae48add9d6c235d
SeanJA <sean.sandy@rtraction.com>
2016-03-14 14:34:54 -0400

    base controller, gets rid of a bunch of code duplication

---
+++ src/Controllers/Controller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Masterclass\Controllers;
+
+
+use PDO;
+
+class Controller
+{
+    /**
+     * @var PDO
+     */
+    protected $db;
+
+    /**
+     * Controller constructor.
+     * @param $config
+     */
+    public function __construct($config) {
+        $dbconfig = $config['database'];
+        $dsn = 'mysql:host=' . $dbconfig['host'] . ';dbname=' . $dbconfig['name'];
+        $this->db = new PDO($dsn, $dbconfig['user'], $dbconfig['pass']);
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    }
+}
