bf26d3d20443bbe45eb17f03487a8bb3ac8ecd1d
SeanJA <sean.sandy@rtraction.com>
2016-03-14 14:29:35 -0400

    moved routes outside of config because the config is in gitignore, so it won't work if someone else pulls down the repo

---
+++ routes.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    '' => 'Masterclass\Controllers\Index:index',
+    'story' => 'Masterclass\Controllers\Story:index',
+    'story/create' => 'Masterclass\Controllers\Story:create',
+    'comment/create' => 'Masterclass\Controllers\Comment:create',
+    'user/create' => 'Masterclass\Controllers\User:create',
+    'user/account' => 'Masterclass\Controllers\User:account',
+    'user/login' => 'Masterclass\Controllers\User:login',
+    'user/logout' => 'Masterclass\Controllers\User:logout',
+];
