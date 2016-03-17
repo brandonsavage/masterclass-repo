178851a64a53e58cf8cedffc7715cf2d408b17e9
aasher <aasher@socious.com>
2016-03-17 13:14:28 -0700

    lesson1
    make it autoload

--- src/Controller/User.php
+++ src/Controller/User.php
@@ -1,54 +1,58 @@
 <?php
+namespace Masterclass\Controller;
 
-class User {
-    
+use PDO;
+
+class User
+{
+
     public $db;
-    
-    public function __construct($config) {
-        $dbconfig = $config['database'];
-        $dsn = 'mysql:host=' . $dbconfig['host'] . ';dbname=' . $dbconfig['name'];
-        $this->db = new PDO($dsn, $dbconfig['user'], $dbconfig['pass']);
-        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
     }
-    
-    public function create() {
+
+    public function create()
+    {
         $error = null;
-        
+
         // Do the create
-        if(isset($_POST['create'])) {
-            if(empty($_POST['username']) || empty($_POST['email']) ||
-               empty($_POST['password']) || empty($_POST['password_check'])) {
+        if (isset($_POST['create'])) {
+            if (empty($_POST['username']) || empty($_POST['email']) ||
+                empty($_POST['password']) || empty($_POST['password_check'])
+            ) {
                 $error = 'You did not fill in all required fields.';
             }
-            
-            if(is_null($error)) {
-                if(!filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)) {
+
+            if (is_null($error)) {
+                if (!filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)) {
                     $error = 'Your email address is invalid';
                 }
             }
-            
-            if(is_null($error)) {
-                if($_POST['password'] != $_POST['password_check']) {
+
+            if (is_null($error)) {
+                if ($_POST['password'] != $_POST['password_check']) {
                     $error = "Your passwords didn't match.";
                 }
             }
-            
-            if(is_null($error)) {
+
+            if (is_null($error)) {
                 $check_sql = 'SELECT * FROM user WHERE username = ?';
                 $check_stmt = $this->db->prepare($check_sql);
                 $check_stmt->execute(array($_POST['username']));
-                if($check_stmt->rowCount() > 0) {
+                if ($check_stmt->rowCount() > 0) {
                     $error = 'Your chosen username already exists. Please choose another.';
                 }
             }
-            
-            if(is_null($error)) {
+
+            if (is_null($error)) {
                 $params = array(
                     $_POST['username'],
                     $_POST['email'],
                     md5($_POST['username'] . $_POST['password']),
                 );
-            
+
                 $sql = 'INSERT INTO user (username, email, password) VALUES (?, ?, ?)';
                 $stmt = $this->db->prepare($sql);
                 $stmt->execute($params);
@@ -57,7 +61,7 @@ class User {
             }
         }
         // Show the create form
-        
+
         $content = '
             <form method="post">
                 ' . $error . '<br />
@@ -68,39 +72,40 @@ class User {
                 <input type="submit" name="create" value="Create User" />
             </form>
         ';
-        
-        require_once 'layout.phtml';
-        
+
+        require_once __DIR__.'/../../layout.phtml';
+
     }
-    
-    public function account() {
+
+    public function account()
+    {
         $error = null;
-        if(!isset($_SESSION['AUTHENTICATED'])) {
+        if (!isset($_SESSION['AUTHENTICATED'])) {
             header("Location: /user/login");
             exit;
         }
-        
-        if(isset($_POST['updatepw'])) {
-            if(!isset($_POST['password']) || !isset($_POST['password_check']) ||
-               $_POST['password'] != $_POST['password_check']) {
-                $error = 'The password fields were blank or they did not match. Please try again.';       
-            }
-            else {
+
+        if (isset($_POST['updatepw'])) {
+            if (!isset($_POST['password']) || !isset($_POST['password_check']) ||
+                $_POST['password'] != $_POST['password_check']
+            ) {
+                $error = 'The password fields were blank or they did not match. Please try again.';
+            } else {
                 $sql = 'UPDATE user SET password = ? WHERE username = ?';
                 $stmt = $this->db->prepare($sql);
                 $stmt->execute(array(
-                   md5($_SESSION['username'] . $_POST['password']), // THIS IS NOT SECURE. 
-                   $_SESSION['username'],
+                    md5($_SESSION['username'] . $_POST['password']), // THIS IS NOT SECURE.
+                    $_SESSION['username'],
                 ));
                 $error = 'Your password was changed.';
             }
         }
-        
+
         $dsql = 'SELECT * FROM user WHERE username = ?';
         $stmt = $this->db->prepare($dsql);
         $stmt->execute(array($_SESSION['username']));
         $details = $stmt->fetch(PDO::FETCH_ASSOC);
-        
+
         $content = '
         ' . $error . '<br />
         
@@ -113,33 +118,33 @@ class User {
             <label>Password Again</label> <input type="password" name="password_check" value="" /><br />
             <input type="submit" name="updatepw" value="Create User" />
         </form>';
-        
-        require_once 'layout.phtml';
+
+        require_once __DIR__.'/../../layout.phtml';
     }
-    
-    public function login() {
+
+    public function login()
+    {
         $error = null;
         // Do the login
-        if(isset($_POST['login'])) {
+        if (isset($_POST['login'])) {
             $username = $_POST['user'];
             $password = $_POST['pass'];
             $password = md5($username . $password); // THIS IS NOT SECURE. DO NOT USE IN PRODUCTION.
             $sql = 'SELECT * FROM user WHERE username = ? AND password = ? LIMIT 1';
             $stmt = $this->db->prepare($sql);
             $stmt->execute(array($username, $password));
-            if($stmt->rowCount() > 0) {
-               $data = $stmt->fetch(PDO::FETCH_ASSOC); 
-               session_regenerate_id();
-               $_SESSION['username'] = $data['username'];
-               $_SESSION['AUTHENTICATED'] = true;
-               header("Location: /");
-               exit;
-            }
-            else {
+            if ($stmt->rowCount() > 0) {
+                $data = $stmt->fetch(PDO::FETCH_ASSOC);
+                session_regenerate_id();
+                $_SESSION['username'] = $data['username'];
+                $_SESSION['AUTHENTICATED'] = true;
+                header("Location: /");
+                exit;
+            } else {
                 $error = 'Your username/password did not match.';
             }
         }
-        
+
         $content = '
             <form method="post">
                 ' . $error . '<br />
@@ -148,12 +153,13 @@ class User {
                 <input type="submit" name="login" value="Log In" />
             </form>
         ';
-        
-        require_once('layout.phtml');
-        
+
+        require_once(__DIR__.'/../../layout.phtml');
+
     }
-    
-    public function logout() {
+
+    public function logout()
+    {
         // Log out, redirect
         session_destroy();
         header("Location: /");
