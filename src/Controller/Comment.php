59e4c3e8265822ca1cca9d0c0687cc91a77b81eb
Amber Himes <amber.himes@lullabot.com>
2016-03-18 00:17:42 -0700

    Homework #2. Refactor sql into models.

--- src/Controller/Comment.php
+++ src/Controller/Comment.php
@@ -2,32 +2,28 @@
 
 namespace Masterclass\Controller;
 
-use PDO;
+use Masterclass\Model\Comment as CommentModel;
 
 class Comment {
+
+  /**
+   * @var CommentModel
+   */
+  protected $commentModel;
     
-    public function __construct($config) {
-        $dbconfig = $config['database'];
-        $dsn = 'mysql:host=' . $dbconfig['host'] . ';dbname=' . $dbconfig['name'];
-        $this->db = new PDO($dsn, $dbconfig['user'], $dbconfig['pass']);
-        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  public function __construct($config) {
+    $this->commentModel = new CommentModel($config);
+  }
+
+  public function create() {
+    if(!isset($_SESSION['AUTHENTICATED'])) {
+      header("Location: /");
+      exit;
     }
-    
-    public function create() {
-        if(!isset($_SESSION['AUTHENTICATED'])) {
-            die('not auth');
-            header("Location: /");
-            exit;
-        }
-        
-        $sql = 'INSERT INTO comment (created_by, created_on, story_id, comment) VALUES (?, NOW(), ?, ?)';
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute(array(
-            $_SESSION['username'],
-            $_POST['story_id'],
-            filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
-        ));
-        header("Location: /story/?id=" . $_POST['story_id']);
-    }
+
+    $this->commentModel->postNewComment($_SESSION['username'], $_POST['story_id'], $_POST['comment']);
+
+    header("Location: /story/?id=" . $_POST['story_id']);
+  }
     
 }
--- src/Controller/Index.php
+++ src/Controller/Index.php
@@ -2,48 +2,45 @@
 
 namespace Masterclass\Controller;
 
-use PDO;
+use Masterclass\Model\Story;
  
 class Index {
-    
-    protected $db;
 
-    protected $config;
+  /**
+   * @var \Masterclass\Model\Story
+   */
+  protected $model;
+
+  /**
+   * @var array
+   */
+  protected $config;
+
+  public function __construct($config) {
+    $this->config = $config;
+    $this->model = new Story($config);
+  }
     
-    public function __construct($config) {
-        $this->config = $config;
-        $dbconfig = $config['database'];
-        $dsn = 'mysql:host=' . $dbconfig['host'] . ';dbname=' . $dbconfig['name'];
-        $this->db = new PDO($dsn, $dbconfig['user'], $dbconfig['pass']);
-        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  public function index() {
+
+    $stories = $this->model->getStoryList();
+
+    $content = '<ol>';
+
+    foreach($stories as $story) {
+
+        $content .= '
+            <li>
+            <a class="headline" href="' . $story['url'] . '">' . $story['headline'] . '</a><br />
+            <span class="details">' . $story['created_by'] . ' | <a href="/story/?id=' . $story['id'] . '">' . $story['count'] . ' Comments</a> |
+            ' . date('n/j/Y g:i a', strtotime($story['created_on'])) . '</span>
+            </li>
+        ';
     }
-    
-    public function index() {
-        
-        $sql = 'SELECT * FROM story ORDER BY created_on DESC';
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute();
-        $stories = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        $content = '<ol>';
-        
-        foreach($stories as $story) {
-            $comment_sql = 'SELECT COUNT(*) as `count` FROM comment WHERE story_id = ?';
-            $comment_stmt = $this->db->prepare($comment_sql);
-            $comment_stmt->execute(array($story['id']));
-            $count = $comment_stmt->fetch(PDO::FETCH_ASSOC);
-            $content .= '
-                <li>
-                <a class="headline" href="' . $story['url'] . '">' . $story['headline'] . '</a><br />
-                <span class="details">' . $story['created_by'] . ' | <a href="/story/?id=' . $story['id'] . '">' . $count['count'] . ' Comments</a> | 
-                ' . date('n/j/Y g:i a', strtotime($story['created_on'])) . '</span>
-                </li>
-            ';
-        }
-        
-        $content .= '</ol>';
-        
-        require $this->config['path'] . '/layout.phtml';
-    }
+
+    $content .= '</ol>';
+
+    require $this->config['path'] . '/layout.phtml';
+  }
 }
 
